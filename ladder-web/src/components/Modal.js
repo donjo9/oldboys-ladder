b26c92e57e8e1926bed7554950862af5a62ce544
Johnni Mortensen <jm@lscontrol.dk>
2019-06-28 15:05:51 +0200

    Added Modal component

---
+++ ladder-web/src/components/Modal.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from "styled-components";
+
+const ModalBackdropContainer = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 999;
+    background-color: #7676767d;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ModalContentContainer = styled.div`
+    position: relative;
+    width: 80%;
+    max-width: 400px;
+    padding: 25px 10px;
+    border-radius: 25px;
+    background-color: var(--background);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    box-shadow: 0 0 5px 0 var(--border-hightlight);
+`;
+
+const Modal = props => {
+    return (
+        <>
+            {props.visable ? (
+                <ModalBackdropContainer onClick={props.dismiss}>
+                    <ModalContentContainer onClick={(e) => e.stopPropagation()}>
+                        {props.children}
+                    </ModalContentContainer>
+                </ModalBackdropContainer>
+            ) : null}
+        </>
+    );
+};
+
+export default Modal;
