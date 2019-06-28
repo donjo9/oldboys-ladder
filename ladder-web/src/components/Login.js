2921440c4a52cf790a8cf80427a6f278f2bf3065
Johnni Mortensen <jm@lscontrol.dk>
2019-06-28 15:06:52 +0200

    changed login to contain design

--- ladder-web/src/components/Login.js
+++ ladder-web/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import CreateUserMutation from "../mutations/CreateUserMutation";
 import LoginUserMutation from "../mutations/LoginUserMutation";
 
@@ -10,34 +11,98 @@ const _submitLoginUser = (email, password) => {
     LoginUserMutation(email, password, log);
 };
 
-const Login = () => {
-    const [name, setName] = useState("");
+const StyledInput = styled.input`
+    width: 250px;
+    padding: 10px 10px;
+    font-size: 1rem;
+    background-color: var(--button-background);
+    border: none;
+    box-shadow: 0 0 5px 0 var(--border-hightlight);
+    margin: 10px;
+    border-radius: 5px;
+    color: white;
+    &::placeholder {
+        color: var(--input-highlight);
+    }
+    &:-webkit-autofill {
+        background: red;
+    }
+`;
+const UserLogin = () => {
     const [password, setPassword] = useState("");
     const [email, setEmail] = useState("");
+
     return (
-        <div>
-            <input
-                type="text"
-                value={name}
-                onChange={e => setName(e.target.value)}
-            />
-            <input
+        <React.Fragment>
+            <StyledInput
                 type="text"
                 value={email}
+                placeholder="Email"
+                name="username"
                 onChange={e => setEmail(e.target.value)}
             />
-            <input
-                type="text"
+            <StyledInput
+                type="password"
+                placeholder="Password"
                 value={password}
                 onChange={e => setPassword(e.target.value)}
             />
             <button onClick={() => _submitLoginUser(email, password)}>
                 Login
             </button>
+        </React.Fragment>
+    );
+};
+
+const CreateUser = () => {
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+
+    return (
+        <React.Fragment>
+            <StyledInput
+                value={name}
+                placeholder="Displayname"
+                type="text"
+                name="displayname"
+                onChange={e => setName(e.target.value)}
+            />
+            <StyledInput
+                value={email}
+                type="email"
+                placeholder="Email"
+                name="username"
+                onChange={e => setEmail(e.target.value)}
+            />
+            <StyledInput
+                value={password}
+                placeholder="Password"
+                type="password"
+                onChange={e => setPassword(e.target.value)}
+            />
             <button onClick={() => _submitCreateUser(name, email, password)}>
                 CreateUser
             </button>
-        </div>
+        </React.Fragment>
+    );
+};
+const Login = () => {
+    const [login, setLogin] = useState(true);
+    return (
+        <React.Fragment>
+            {login ? (
+                <React.Fragment>
+                    <UserLogin />
+                    <button onClick={() => setLogin(false)}>New user</button>
+                </React.Fragment>
+            ) : (
+                <React.Fragment>
+                    <CreateUser />
+                    <button onClick={() => setLogin(true)}>Have user</button>
+                </React.Fragment>
+            )}
+        </React.Fragment>
     );
 };
 
