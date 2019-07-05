0127f4f235c5340df4e91aff93e11154809e2346
Johnni Mortensen <jm@lscontrol.local>
2019-07-05 09:51:47 +0200

    Token check on Profile If no token found then redirect to /

--- ladder-web/src/components/Profile.js
+++ ladder-web/src/components/Profile.js
@@ -1,37 +1,46 @@
-import React from "react";
+import React, { useContext } from "react";
 import graphql from "babel-plugin-relay/macro";
 import { QueryRenderer } from "react-relay";
+import { Redirect } from "react-router-dom";
 import environment from "../Environment";
+import { LoginContext } from "./context";
 
 const Profile = props => {
+    const { state } = useContext(LoginContext);
     return (
-        <QueryRenderer
-            environment={environment}
-            query={graphql`
-                query ProfileQuery {
-                    me {
-                        name
-                        email
-                    }
-                }
-            `}
-            variables={{}}
-            render={({ error, props }) => {
-                if (error) {
-                    console.log(error);
-                    return <div>Error!!</div>;
-                }
-                if (!props) {
-                    return <div>Loading...</div>;
-                }
-                return (
-                    <div>
-                        <div>{props.me.name}</div>
-                        <div>{props.me.email}</div>
-                    </div>
-                );
-            }}
-        />
+        <React.Fragment>
+            {state.token ? (
+                <QueryRenderer
+                    environment={environment}
+                    query={graphql`
+                        query ProfileQuery {
+                            me {
+                                name
+                                email
+                            }
+                        }
+                    `}
+                    variables={{}}
+                    render={({ error, props }) => {
+                        if (error) {
+                            console.log(error);
+                            return <div>Error!!</div>;
+                        }
+                        if (!props) {
+                            return <div>Loading...</div>;
+                        }
+                        return (
+                            <div>
+                                <div>{props.me.name}</div>
+                                <div>{props.me.email}</div>
+                            </div>
+                        );
+                    }}
+                />
+            ) : (
+                <Redirect to="/" />
+            )}
+        </React.Fragment>
     );
 };
 export default Profile;
